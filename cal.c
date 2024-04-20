7d2d5225f185dd3672f86694fb43dff24a8f4180
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-04-19 20:21:44 -0500

    Add myMul for bitwise multiple example
    -  using myAdd under the hood

--- cal.c
+++ cal.c
@@ -21,6 +21,24 @@ int mySub(int x, int y){
     return myAdd(x, negativeY);
 }
 
+int myMul(int x, int y){
+    int sum = 0;
+
+    if (y > 0){
+        for(int i = 0; i < y; i++){
+            sum = myAdd(sum, x);
+        }
+    }else{
+        y = myAdd(~(y), 1);
+        for(int i = 0; i < y; i++){
+            sum = myAdd(sum, x);
+        }
+        sum = myAdd(~(sum), 1);
+    }
+
+    return sum;
+}
+
 int main(int argc, char* argv[]){
     int x, y;
 
@@ -35,6 +53,9 @@ int main(int argc, char* argv[]){
 
     int z2 = mySub(x, y);
     printf("x - y = %d\n", z2);
+
+    int z3 = myMul(x, y);
+    printf("x * y = %d\n", z3);
 
     return 0;
 }
