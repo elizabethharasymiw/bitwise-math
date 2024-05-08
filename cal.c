aa29d5736af1a09f857fc9dedc3807895f262a0b
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-05-07 23:37:54 -0500

    Fixed seg fault in threadds, but now math is wrong for div and mod

--- cal.c
+++ cal.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
 int MAX_INT = 2147483647;
@@ -6,6 +7,7 @@ int MAX_INT = 2147483647;
 typedef struct{
     int x;
     int y;
+    int result;
 } ThreadArgs;
 
 int myAdd(int x, int y);
@@ -23,19 +25,22 @@ int main(int argc, char* argv[]){
     int x, y;
     int divResult;
     int modResult;
-    ThreadArgs threadArgs;
+    ThreadArgs threadArgsDiv, threadArgsMod;
+    int *result_div, *result_mod;
 
     getIntFromUser(&x, "x");
     getIntFromUser(&y, "y");
 
-    threadArgs.x = x;
-    threadArgs.y = y;
+    threadArgsDiv.x = x;
+    threadArgsDiv.x = x;
+    threadArgsMod.y = y;
+    threadArgsMod.y = y;
 
     pthread_t div_thread_id;
     pthread_t mod_thread_id;
 
-    pthread_create(&div_thread_id, NULL, divThread, (void*) &threadArgs);
-    pthread_create(&mod_thread_id, NULL, divThread, (void*) &threadArgs);
+    pthread_create(&div_thread_id, NULL, divThread, (void*) &threadArgsDiv);
+    pthread_create(&mod_thread_id, NULL, divThread, (void*) &threadArgsMod);
 
     printf("x = %d\n", x);
     printf("y = %d\n", y);
@@ -43,10 +48,10 @@ int main(int argc, char* argv[]){
     printf("x + y = %d\n", myAdd(x, y));
     printf("x - y = %d\n", mySub(x, y));
     printf("x * y = %d\n", myMul(x, y));
-    pthread_join(div_thread_id, divResult);
-    printf("x / y = %d\n", divResult);
-    pthread_join(mod_thread_id, modResult);
-    printf("x %% y = %d\n", modResult);
+    pthread_join(div_thread_id, (void **)&result_div);
+    printf("x / y = %d\n", *result_div);
+    pthread_join(mod_thread_id, (void **)&result_mod);
+    printf("x %% y = %d\n", *result_mod);
 
     return 0;
 }
@@ -173,13 +178,20 @@ void getIntFromUser(int* num, char* name){
 
 void* divThread(void* args){
     ThreadArgs *myargs = (ThreadArgs *) args;
-    int result = myDiv(myargs->x, myargs->y);
-    pthread_exit((void *)result);
+    myargs->result = myDiv(myargs->x, myargs->y);
+
+    int *result_ptr = malloc(sizeof(int));
+    *result_ptr = myargs->result;
+
+    pthread_exit(result_ptr);
 }
 
 void* modThread(void* args){
     ThreadArgs *myargs = (ThreadArgs *) args;
-    int result = myMod(myargs->x, myargs->y);
-    pthread_exit((void *)result);
+    myargs->result = myMod(myargs->x, myargs->y);
 
+    int *result_ptr = malloc(sizeof(int));
+    *result_ptr = myargs->result;
+
+    pthread_exit(result_ptr);
 }
